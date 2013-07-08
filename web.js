05d66b5e02e291c847a03ac59fbf8c4dc8094312
kstefaine <kstefanie@gmail.com>
2013-07-08 00:01:02 -0700

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
+var indexFileName = 'index.html';
 var app = express.createServer(express.logger());
+var indexFileBuf = fs.readFileSync(indexFileName);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexFileBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
